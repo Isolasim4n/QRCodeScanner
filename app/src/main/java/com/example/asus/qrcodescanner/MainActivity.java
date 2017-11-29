4f3556485a7dc4ef65a5920c7d07d11bad128f85
Prasetya <sndr1722@gmail.com>
2017-11-29 18:44:03 +0700

    *QRCodeScanner

---
+++ app/src/main/java/com/example/asus/qrcodescanner/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.asus.qrcodescanner;
+
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+    private ZXingScannerView mScannerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Programmatically initialize the scanner view
+        mScannerView = new ZXingScannerView(this);
+        setContentView(mScannerView);
+    }
+
+    @Override
+    public void handleResult(Result result) {
+        // Do something with the result here
+        Log.v("TAG", result.getText()); // Prints scan results
+        // Prints the scan format (qrcode, pdf417 etc.)
+        Log.v("TAG", result.getBarcodeFormat().toString());
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Scan Result");
+        builder.setMessage(result.getText());
+        AlertDialog alert1 = builder.create();
+        alert1.show();
+
+        // If you would like to resume scanning, call this method below:
+        mScannerView.resumeCameraPreview(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Register ourselves as a handler for scan results.
+        mScannerView.setResultHandler(this);
+        // Start camera on resume
+        mScannerView.startCamera();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Stop camera on pause
+        mScannerView.stopCamera();
+    }
+}
